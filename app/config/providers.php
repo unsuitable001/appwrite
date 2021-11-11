5c341ee4d8121177115aa947315ea687d67263a3
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2021-11-11 13:38:32 +0530

    add KeyCloak to provier list

--- app/config/providers.php
+++ app/config/providers.php
@@ -111,6 +111,16 @@ return [ // Ordered by ABC.
         'beta' => false,
         'mock' => false,
     ],
+    'keycloak' => [
+        'name' => 'KeyCloak',
+        'developers' => 'https://www.keycloak.org/documentation',
+        'icon' => 'icon-keycloak',
+        'enabled' => true,
+        'sandbox' => false,
+        'form' => false,
+        'beta' => false,
+        'mock' => false,
+    ],
     'linkedin' => [
         'name' => 'LinkedIn',
         'developers' => 'https://developer.linkedin.com/',
